0c6e255106e3fed2a3fa8f6ec46bdff2f83e56f9
orzcode <71000311+orzcode@users.noreply.github.com>
2025-01-28 10:19:56 +1300

    ~

--- routes/router.js
+++ routes/router.js
@@ -11,13 +11,13 @@ import { addOrUpdateUser } from "../public/js/userService.js";
 
 //router.get("/", (req, res) => res.send("Hello, world!"));
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // Read the JSON file
-  const filePath = path.join(__dirname, '../public/data/models.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  const filePath = path.join(__dirname, "../public/data/models.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      console.error('Error reading models.json:', err);
-      res.status(500).send('Failed to read models.json');
+      console.error("Error reading models.json:", err);
+      res.status(500).send("Failed to read models.json");
       return;
     }
 
@@ -25,13 +25,13 @@ router.get('/', (req, res) => {
     const motherboards = JSON.parse(data);
 
     // Render the EJS template with the parsed data
-    res.render('allmodels', { motherboards });
+    res.render("allmodels", { motherboards });
   });
 });
 
 router.post("/submit", (req, res) => {
   const { email, selectedMobo } = req.body;
-  
+
   if (!email || !selectedMobo) {
     return res.status(400).send("Missing email or motherboard selection");
   }
@@ -39,7 +39,8 @@ router.post("/submit", (req, res) => {
   addOrUpdateUser(email, selectedMobo);
 
   res.redirect("/");
+  //later add redir page here?
+  //otherwise need some kind of user feedback
 });
-
 
 export default router;
