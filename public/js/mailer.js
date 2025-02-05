4058dce0184f409224e609848bb0a58ac85a4243
orzcode <71000311+orzcode@users.noreply.github.com>
2025-02-05 18:50:45 +1300

    add unsub header url

--- public/js/mailer.js
+++ public/js/mailer.js
@@ -16,6 +16,9 @@ export async function mailer(userObject, modelsObject) {
     to: user.email,
     subject: "New Bios Update Available",
     html: `<strong>A new BIOS is available for your ASRock ${user.mobo} motherboard.</strong> <p>You can download it from <a href="${models.biospage}">the official ASRock page here</a>.</p><br><p>Note: you are receiving this because you previously signed up to be notified of updates from the unofficial <a href="${projectLink}">ASRock Bios Notifier project page</a>.</p><br><p>You can unsubscribe at any time <a href="${unsubLink}">by clicking here!<a></p>`,
+    headers: {
+      'List-Unsubscribe': `${unsubLink}`,
+    },
   });
 
   if (error) {
