77d42fa5252520ed0d8aca5685d6bd3233872daf
orzcode <71000311+orzcode@users.noreply.github.com>
2025-02-14 15:09:59 +1300

    add testMailer function

--- public/js/mailer.js
+++ public/js/mailer.js
@@ -15,7 +15,7 @@ export async function mailer(userObject, modelsObject) {
     from: "ASRock Bios Notifier <onboarding@resend.dev>",
     to: user.email,
     subject: "New Bios Update Available",
-    html: `A new BIOS is available for your ASRock <strong>${user.mobo}</strong> motherboard. <p>You can download it from <a href="${models.biospage}">the official ASRock page here</a>.</p><br><p>Note: you are receiving this because you previously signed up to be notified of updates from the unofficial <a href="${projectLink}">ASRock Bios Notifier project page</a>.</p><br><p>You can unsubscribe at any time <a href="${unsubLink}">by clicking here<a> or by visiting the page and re-entering your email.</p><br>
+    html: `A new BIOS is available for your ASRock <strong>${user.mobo}</strong> motherboard. <p>You can download it from <a href="${models.biospage}">the official ASRock page here</a>.</p><br><p>Note: you are receiving this because you previously signed up to be notified of updates from the unofficial <a href="${projectLink}">ASRock Bios Notifier project page</a>.</p><br><p>You can unsubscribe at any time <a href="${unsubLink}">by clicking here<a> or by visiting the page and re-entering your email.</a></p><br>
     <p>Please note that this is an <strong>unofficial</strong> tool and not supported by ASRock. As with any such service, things can break, fail to update, or change at any time. Long-term reliability is not guaranteed.</p>`,
     headers: {
       "List-Unsubscribe": `${unsubLink}`,
@@ -25,6 +25,17 @@ export async function mailer(userObject, modelsObject) {
   if (error) {
     return console.error({ error });
   }
-  console.log("MAILER.JS:   Email sent to ", user.id);
+  console.log("MAILER.JS: Email sent to ", user.id, " at ", new Date().toISOString());
   console.log({ data });
+}
+
+// Test function with hidden objects from .env
+export async function testMail() {
+  try {
+    const mailTestUser = JSON.parse(process.env.MAIL_TEST_USER);
+    const mailTestModel = JSON.parse(process.env.MAIL_TEST_MODEL);
+    await mailer(mailTestUser, mailTestModel);
+  } catch (err) {
+    console.error("Error parsing test objects from .env", err);
+  }
 }
